f7d159de628aebfd05af394435dbe9c5aa07d3ef
Adam Pacholski <adamp@accretivetg.com>
2015-12-22 22:21:03 -0800

    Ooh now with more flux (adam)

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import App from './components/App';
 
 const mountNode = document.getElementById('root');
 
-ReactDOM.render(
-  <HelloWorld />,
-  mountNode
-);
+ReactDOM.render(<App />, mountNode);
